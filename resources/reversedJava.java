77f8ab7dd21e1ef436e2853211849924892198c2
Mona Lisa <Vvatermelon4ik@gmail.com>
2023-12-17 18:31:04 +0300

    Initial commit

---
+++ resources/reversedJava.java
@@ -0,0 +1,14 @@
+{ pmet ssalc cilbup
+;"olleh" = olleh gnirtS lanif citats cilbup    
+
+;ega tni cilbup    
+;looBemos naeloob cilbup    
+
+{ )(pmet cilbup    
+;)"cilbup"(nltnirp.tuo.metsyS        
+}    
+
+{ )i tni(dohteMtset diov cilbup    
+;)i(nltnirp.tuo.metsyS        
+}    
+}
